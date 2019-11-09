4b748a4349578b0b96fac3c02f5313b5d4db2bc1
Garret <22078283+ALien747@users.noreply.github.com>
2019-11-09 14:06:50 +0100

    0001

--- core/src/de/lostmekka/_3m5gamejam6/GameView.kt
+++ core/src/de/lostmekka/_3m5gamejam6/GameView.kt
@@ -5,33 +5,28 @@ import org.hexworks.zircon.api.component.Button;
 import org.hexworks.zircon.api.component.Panel;
 import org.hexworks.zircon.api.grid.TileGrid;
 import org.hexworks.zircon.api.screen.Screen;
-import org.hexworks.zircon.api.uievent.KeyCode;
-import org.hexworks.zircon.api.uievent.KeyboardEventType;
-import org.hexworks.zircon.api.uievent.MouseEventType;
-import org.hexworks.zircon.api.uievent.UIEventPhase;
 import org.hexworks.zircon.api.component.ComponentAlignment.CENTER
 import org.hexworks.zircon.api.uievent.ComponentEventType.ACTIVATED
 import org.hexworks.zircon.api.Components
 import org.hexworks.zircon.api.Positions
-import org.hexworks.zircon.api.component.Panel
 import org.hexworks.zircon.api.mvc.base.BaseView
-
-
-
-
+import org.hexworks.zircon.internal.application.SwingApplication
+import org.lwjgl.input.Keyboard.KEY_A
+import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.extensions.onMouseEvent
+import org.hexworks.zircon.api.grid.TileGrid
+import org.hexworks.zircon.api.UIEventResponses
+import org.hexworks.zircon.api.uievent.*
 
 
 class GameView : BaseView() {
-    lateinit var sidebar: Panel
     override fun onDock() {
 
-        val tileGrid = SwingApplications.startTileGrid()
-        val screen = Screens.createScreenFor(tileGrid)
+
 
 
         val sidebar = Components.panel()
             .withSize(GameConfig.SIDEBAR_WIDTH, GameConfig.WINDOW_HEIGHT-2)
-
             .wrapWithBox()
             .withTitle("Game Info")
             .withPosition(Positions.offset1x1())
@@ -42,9 +37,14 @@ class GameView : BaseView() {
             .withText("Header")
             .build()
 
-
-
         sidebar.addComponent(header)
         screen.addComponent(sidebar)
+
+        screen.onMouseEvent(MouseEventType.MOUSE_MOVED) { event: MouseEvent, phase: UIEventPhase ->
+            header.text = event.position.toString()
+            UIEventResponses.processed()
+        }
+
+
     }
 }
