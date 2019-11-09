a245182570b4fe94e8fa60d817bdd03524e09bb3
3m5.Dana Kuban <Dana.Kuban@3m5.de>
2019-11-09 18:16:14 +0100

    color madness

--- core/src/de/lostmekka/_3m5gamejam6/GameColors.kt
+++ core/src/de/lostmekka/_3m5gamejam6/GameColors.kt
@@ -4,13 +4,19 @@ import org.hexworks.zircon.api.TileColors
 
 object GameColors {
     val WALL_FOREGROUND = TileColors.fromString("#75715E")
+    val WALL_FOREGROUND_MADNESS = TileColors.fromString("#2f2d22")
     val WALL_BACKGROUND = TileColors.fromString("#3E3D32")
+    val WALL_BACKGROUND_MADNESS = TileColors.fromString("#000000")
 
     val FLOOR_FOREGROUND = TileColors.fromString("#75715E")
+    val FLOOR_FOREGROUND_MADNESS = TileColors.fromString("#4b2b5e")
     val FLOOR_BACKGROUND = TileColors.fromString("#1e2320")
+    val FLOOR_BACKGROUND_MADNESS = TileColors.fromString("#000000")
 
     val ACCENT_COLOR = TileColors.fromString("#FFCD22")
+    val ACCENT_COLOR_MADNESS = TileColors.fromString("#8923c7")
     val ACCENT_COLOR_2 = TileColors.fromString("#00FF00")
+    val ACCENT_COLOR_2_MADNESS = TileColors.fromString("#ff6400")
 
     val MADNESS = TileColors.fromString("#000000")
 }
